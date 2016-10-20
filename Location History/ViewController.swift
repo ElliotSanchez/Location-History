c3335bfb4f9c0fd5cf5bed551cfff2b7a52eab4f
hostname <github@elliotsanchez.com>
2016-10-20 16:20:51 -0500

    Map with nav bar that displays location after checking for permissions

--- Location History/ViewController.swift
+++ Location History/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    // Outlet for fullscreen map
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    @IBAction func addLocationTapped(_ sender: AnyObject) {
+        let coord = locationManager.location?.coordinate
+        
+        if let lat = coord?.latitude {
+            print("Lattitude: " + String(lat))
+        }
+        
+        if let long = coord?.longitude {
+            print("longitude: " + String(long))
+        }
+    }
+    
+    
+    // Manager needs to globally accessible to view, so not in viewDidLoad
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Location manager functions should be performed in ViewController, because it is the class that inherits from CLLocationManagerDelegate
+        locationManager.delegate = self
+        
+        // Ask and then update location upon loading
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
+    // Update map or display warning upon change of location authorization
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status == .authorizedWhenInUse else {
+            // In practice, this would be a message to the user, rather than printed to the console.
+            print("The app won't work without access to your location.")
+            return
+        }
+        
+        // Update map when location access is granted
+        print("Location access granted, thanks.")
+        myMapView.showsUserLocation = true
+        
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
